b9a463a0e748ed09cff524dadbc3d61fb48078a3
herman <hermanpineda94@gmail.com>
2020-10-04 20:08:34 -0500

    implemented communicatio between fleet and car services

--- car-service/car-service.js
+++ car-service/car-service.js
@@ -1,38 +1,71 @@
 const express = require("express");
 const app = express();
+const axios = require('axios');
 const bodyParser = require("body-parser");
-const path = require("path");
 const port = process.argv.slice(2)[0];
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+
+const fleetService = "http://localhost:5001";
 
 const taxis = [
-  { id: 1, zip: "60606", location: "West Loop Gate", name: "Yellow Cab", offduty: false, occupied: false },
-  { id: 2, zip: "60640", location: "Edgewater", name: "Flash Cab", offduty: false, occupied: false },
-  { id: 3, zip: "60666", location: "O'Hare", name: "O'Hare Taxi", offduty: false, occupied: false },
-  { id: 4, zip: "60654", location: "River North", name: "City Taxi", offduty: false, occupied: false },
+  { id: 1, zip: "60606"},
+  { id: 2, zip: "60640"},
+  { id: 3, zip: "60666"},
+  { id: 4, zip: "60654"}
 ];
 
+// @route   GET /taxis
+// @desc    Get taxis.
+// @access  Public
 app.get("/taxis", (req, res) => {
   console.log("Returning taxi list...");
+  console.log(taxis);
   res.send(taxis);
 });
 
-app.post("/taxi/**", (req, res) => {
-  const taxiId = parseInt(req.params[0]);
-  const foundTaxi = taxis.find((subject) => subject.id === taxiId);
+// @route   POST /taxi/**
+// @desc    Update a taxi car attribute.
+//          If taxi car zip changes, then
+//          fleet service must know.
+// @access  Public
+app.post("/taxi/**", async (req, res) => {
+  const taxiId      = parseInt(req.params[0]);
+  const foundTaxi   = taxis.find((subject) => subject.id === taxiId);
+  let   zipModified = false;
   if (foundTaxi) {
     for (let attribute in foundTaxi) {
       if (req.body[attribute]) {
-        foundTaxi[attribute] = req.body[attribute];
+        let value = req.body[attribute];
+        if ((attribute === "zip") && (foundTaxi.zip !== value)) {
+          zipModified = true;
+        }
+        foundTaxi[attribute] = value;
         console.log(
           `Set ${attribute} to ${req.body[attribute]} in taxi: ${taxiId}.`
         );
       }
     }
-    res
-      .status(202)
-      .header({ Location: `http://localhost:${port}/taxi/${foundTaxi.id}` })
-      .send(foundTaxi);
+
+    if (zipModified) {
+      try {
+        const resp = await axios.post(`${fleetService}/taxi/${taxiId}`, 
+        { zip: `${foundTaxi.zip}`}, 
+        {headers: { 'Content-Type': 'application/json' }});
+        res
+          .status(202)
+          .header({ Location: `http://localhost:${port}/taxi/${foundTaxi.id}` })
+          .send(foundTaxi);
+        
+      } catch (error) {
+            console.log("Error updating fleet service.");
+            res.status(404).send(error);
+      }
+    } else {
+      res
+        .status(202)
+        .header({ Location: `http://localhost:${port}/taxi/${foundTaxi.id}` })
+        .send(foundTaxi);
+    }
   } else {
     console.log("Taxi not found.");
     res.status(404).send();
--- fleet-service/fleet-service.js
+++ fleet-service/fleet-service.js
@@ -4,36 +4,33 @@ const bodyParser = require("body-parser");
 const port = process.argv.slice(2)[0];
 app.use(bodyParser.json());
 
-const carService = "http://localhost:5000/";
-
 const taxiOrders = [
-    { id: 1, zip: "60077", location: "Skokie", assignedTaxi: 0 },
-    { id: 2, zip: "60637", location: "Washington Park", assignedTaxi: 0 },
-    { id: 3, zip: "60657", location: "Lakeview East", assignedTaxi: 0}
+    // { id: 1, zip: "60077"},
+    // { id: 2, zip: "60637"},
+    // { id: 3, zip: "60657"}
 ];
+
+const taxis = [];
+
 let orderCount = taxiOrders.length;
+let taxiId     = taxis.length;
 
 // @route   GET /taxiorders
 // @desc    Get list of existing taxi orders.
 // @access  Public
 app.get("/taxiorders", (req, res) => {
     console.log("Returning taxi orders...");
+    console.log(taxiOrders);
     res.send(taxiOrders);
 });
 
-// @route   GET /taxiorder/:id
-// @desc    Get existing taxi order.
+// @route   GET /taxis
+// @desc    Get fleet of taxis.
 // @access  Public
-app.get("/taxiorder/:id", (req, res) => {
-    const orderId = parseInt(req.params.id);
-    const foundOrder = taxiOrders.find(subject => subject.id === orderId);
-    if (foundOrder) {
-        console.log(`Returning taxi order ${orderId}...`);
-        res.status(202).send(foundOrder);
-    } else {
-        console.log("Taxi order not found.");
-        res.status(404).send();
-    }
+app.get("/taxis", (req, res) => {
+    console.log("Returning taxi fleet...");
+    console.log(taxis);
+    res.send(taxis);
 });
 
 // @route   POST /order
@@ -42,16 +39,14 @@ app.get("/taxiorder/:id", (req, res) => {
 // @access  Public
 app.post("/order/", (req, res) => {
     console.log("Creating taxi order...");
-    const { zip, location } = req.body;
-    if (!zip || !location) {
+    const zip  = req.body["zip"];
+    if (!zip) {
         console.log("Failed to add new order.");
-        res.status(404).send("Please provide zip and location.");
+        res.status(404).send("Please provide zip.");
     } else {
         const newOrder = {
             id: ++orderCount,
-            zip,
-            location,
-            assignedTaxi: 0
+            zip
         };
 
         taxiOrders.push(newOrder);
@@ -62,24 +57,46 @@ app.post("/order/", (req, res) => {
     }
 });
 
-// @route   POST /order/:id
-// @desc    Update a taxi order.
+// @route   POST /taxi
+// @desc    Create a taxi to add to fleet.
+//          POST because server sets identifier.
 // @access  Public
-app.post("/order/:id", (req, res) => {
-    const orderId = parseInt(req.params.id);
-    const foundOrder = taxiOrders.find(subject => subject.id === orderId);
-    if (foundOrder) {
-        for (let attribute in foundOrder) {
+app.post("/taxi/", (req, res) => {
+    const zip  = req.body["zip"];
+    if (!zip) {
+        console.log("Failed to add new taxi.");
+        res.status(404).send("Please provide zip.");
+    } else {
+        const newTaxi = {
+            id: ++taxiId,
+            zip,
+            busy: false
+        };
+
+        taxis.push(newTaxi);
+        console.log(`Added new taxi: ${JSON.stringify(newTaxi)}`);
+        res.status(202)
+        .header({ Location: `http://localhost:${port}/taxi/${orderCount}` })
+        .send(newTaxi);
+    }
+});
+
+// @route   POST /taxi/:id
+// @desc    Update a fleet taxi's location.
+// @access  Public
+app.post("/taxi/:id", (req, res) => {
+    const taxiId = parseInt(req.params.id);
+    const foundTaxi = taxis.find(subject => subject.id === taxiId);
+    if (foundTaxi) {
+        for (let attribute in foundTaxi) {
             if (req.body[attribute]) {
-              foundOrder[attribute] = req.body[attribute];
+              foundTaxi[attribute] = req.body[attribute];
               console.log(
-                `Set ${attribute} to ${req.body[attribute]} in order: ${orderId}.`
+                `Updated ${attribute} to ${req.body[attribute]} in taxi: ${taxiId}.`
               );
             }
         }
-        res.status(202)
-        .header({ Location: `http://localhost:${port}/taxiorder/${foundOrder.id}` })
-        .send(foundOrder);
+        res.status(202).send();
     } else {
         console.log("Taxi order not found.");
         res.status(404).send();
